1a2228f3a1dfc083a4e51a574457d011bea5478e
pedropsybyloski <pedropsybyloski@gmail.com>
2021-12-02 03:05:15 -0300

    Commit heroku

--- DB/mysql.js
+++ DB/mysql.js
@@ -7,6 +7,7 @@ const sequelize = new Sequelize(
     process.env.MYSQL_USER,
     process.env.MYSQL_PASSWORD,
     {
+        host: process.env.MYSQL_HOST,
         dialect: "mysql",
         port: process.env.MYSQL_PORT
     }
