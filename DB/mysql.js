44a6f30c8281370b61eadffe80422f84e881747b
pedropsybyloski <pedropsybyloski@gmail.com>
2022-04-01 01:45:28 -0300

    teste heroku

--- DB/mysql.js
+++ DB/mysql.js
@@ -4,21 +4,21 @@ dotenv.config();
 
 let sequelize;
 
-/* if(process.env.CLEARDB_DATABASE_URL){
+if(process.env.CLEARDB_DATABASE_URL){
     sequelize = new Sequelize(process.env.CLEARDB_DATABASE_URL);
 } else{
-} */
-sequelize = new Sequelize(
-    process.env.MYSQL_DB,
-    process.env.MYSQL_USER,
-    process.env.MYSQL_PASSWORD,
-    {
-        host: process.env.MYSQL_HOST,
-        dialect: "mysql",
-        port: process.env.MYSQL_PORT,
-        timezone: '-03:00'
-    }
-);
+    sequelize = new Sequelize(
+        process.env.MYSQL_DB,
+        process.env.MYSQL_USER,
+        process.env.MYSQL_PASSWORD,
+        {
+            host: process.env.MYSQL_HOST,
+            dialect: "mysql",
+            port: process.env.MYSQL_PORT,
+            timezone: '-03:00'
+        }
+    );
+}
 
 sequelize.authenticate().then(()=>{
     console.log("Connection to MYSQL successful!");
